3cfb7c04f8453ad9f1df0ec470afbc3a80cd3e98
Ahmed Sayed <therealahme@gmail.com>
2023-12-11 19:02:31 +0200

    disable foreign keys check

--- DB/DBSync.js
+++ DB/DBSync.js
@@ -1,17 +1,27 @@
 /* eslint-disable no-unused-vars */
-const sequelize = require('./DBConnection')
-const User = require('../models/User')
-const Patient = require('../models/Patient')
-const Doctor = require('../models/Doctor')
-const AvailabilitySchedule = require('../models/AvailabilitySchedule')
-const Appointment = require('../models/Appointment')
-const Billing = require('../models/Billing')
-const Inventory = require('../models/Inventory')
-const MedicalRecord = require('../models/MedicalRecord')
-
-
-sequelize.sync({ force: true }).then(() => {
-  console.log('Database synchronized');
-}).catch((err) => {
-  console.error('Error synchronizing database:', err);
-});
+const sequelize = require('./DBConnection');
+const User = require('../models/User');
+const Patient = require('../models/Patient');
+const Doctor = require('../models/Doctor');
+const AvailabilitySchedule = require('../models/AvailabilitySchedule');
+const Appointment = require('../models/Appointment');
+const Billing = require('../models/Billing');
+const Inventory = require('../models/Inventory');
+const MedicalRecord = require('../models/MedicalRecord');
+// Disable foreign key checks
+sequelize.query('SET foreign_key_checks = 0', { raw: true })
+  .then(() =>
+    // Sync all models
+    sequelize.sync({ force: true })
+  )
+  .then(() => {
+    console.log('Database synchronized');
+    // Re-enable foreign key checks
+    return sequelize.query('SET foreign_key_checks = 1', { raw: true });
+  })
+  .then(() => {
+    console.log('Foreign key checks re-enabled.');
+  })
+  .catch((err) => {
+    console.error('Error synchronizing database:', err);
+  });
