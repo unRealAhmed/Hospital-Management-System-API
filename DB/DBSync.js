f146cde97ba8931df8687e9fb4570f456cc762dd
Ahmed Sayed <therealahme@gmail.com>
2023-12-11 14:47:56 +0200

    sync all models

--- DB/DBSync.js
+++ DB/DBSync.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-unused-vars */
 const sequelize = require('./DBConnection')
+const User = require('../models/User')
+const Patient = require('../models/Patient')
+const Doctor = require('../models/Doctor')
+const AvailabilitySchedule = require('../models/AvailabilitySchedule')
+const Appointment = require('../models/Appointment')
+const Billing = require('../models/Billing')
+const Inventory = require('../models/Inventory')
+const MedicalRecord = require('../models/MedicalRecord')
 
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   console.log('Database synchronized');
 }).catch((err) => {
   console.error('Error synchronizing database:', err);
--- DB/tables.sql
+++ DB/tables.sql
@@ -1,129 +1,112 @@
 -- Patient
 
 CREATE TABLE Patients (
-  patientId SERIAL PRIMARY KEY,
+  patientId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  age INTEGER NOT NULL,
+  age INT UNSIGNED NOT NULL,
   gender VARCHAR(255) NOT NULL,
   contactInformation VARCHAR(255) NOT NULL,
   medicalHistory TEXT,
-  appointmentId INTEGER REFERENCES Appointments(id),
-  medicalRecordId INTEGER REFERENCES MedicalRecords(id),
-  userId INTEGER REFERENCES Users(userId)
+  appointmentId INT UNSIGNED,
+  medicalRecordId INT UNSIGNED,
+  userId INT UNSIGNED,
+  FOREIGN KEY (appointmentId) REFERENCES Appointments(appointmentId),
+  FOREIGN KEY (medicalRecordId) REFERENCES MedicalRecords(recordId),
+  FOREIGN KEY (userId) REFERENCES Users(userId)
 );
 
+ 
 
-ALTER TABLE Patients
-  ADD CONSTRAINT fk_patient_appointment FOREIGN KEY (appointmentId) REFERENCES Appointments(id);
-
-ALTER TABLE Patients
-  ADD CONSTRAINT fk_patient_medical_record FOREIGN KEY (medicalRecordId) REFERENCES MedicalRecords(id);
-
-ALTER TABLE Patients
-  ADD CONSTRAINT fk_patient_user FOREIGN KEY (userId) REFERENCES Users(userId);
-
---  Doctor
+-- Doctor
 
 CREATE TABLE Doctors (
-  doctorId SERIAL PRIMARY KEY,
+  doctorId INT AUTO_INCREMENT PRIMARY KEY, 
   name VARCHAR(255) NOT NULL,
   specialization VARCHAR(255) NOT NULL,
   contactInformation VARCHAR(255) NOT NULL,
-  appointmentId INTEGER REFERENCES Appointments(id),
-  availabilityScheduleId INTEGER REFERENCES AvailabilitySchedules(id),
-  userId INTEGER REFERENCES Users(userId)
+  appointmentId INT,
+  availabilityScheduleId INT,
+  userId INT,
+  FOREIGN KEY (appointmentId) REFERENCES Appointments(appointmentId),
+  FOREIGN KEY (availabilityScheduleId) REFERENCES AvailabilitySchedules(availabilityId),
+  FOREIGN KEY (userId) REFERENCES Users(userId)
 );
 
-ALTER TABLE Doctors
-  ADD CONSTRAINT fk_doctor_appointment FOREIGN KEY (appointmentId) REFERENCES Appointments(id);
-
-ALTER TABLE Doctors
-  ADD CONSTRAINT fk_doctor_availability_schedule FOREIGN KEY (availabilityScheduleId) REFERENCES AvailabilitySchedules(id);
-
-ALTER TABLE Doctors
-  ADD CONSTRAINT fk_doctor_user FOREIGN KEY (userId) REFERENCES Users(userId);
+ 
 
 -- User
 
 CREATE TABLE Users (
-  userId SERIAL PRIMARY KEY,
+  userId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   role VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE Patients
-  ADD CONSTRAINT fk_patient_user FOREIGN KEY (userId) REFERENCES Users(userId);
+ 
 
-ALTER TABLE Doctors
-  ADD CONSTRAINT fk_doctor_user FOREIGN KEY (userId) REFERENCES Users(userId);
-
--- Appoointment
+-- Appointment
 
 CREATE TABLE Appointments (
-  appointmentId SERIAL PRIMARY KEY,
+  appointmentId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   date DATE NOT NULL,
   time TIME NOT NULL,
   status VARCHAR(255) NOT NULL,
-  patientId INTEGER REFERENCES Patients(patientId),
-  doctorId INTEGER REFERENCES Doctors(doctorId)
+  patientId INT UNSIGNED,
+  doctorId INT UNSIGNED,
+  FOREIGN KEY (patientId) REFERENCES Patients(patientId),
+  FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId)
 );
 
-ALTER TABLE Appointments
-  ADD CONSTRAINT fk_appointment_patient FOREIGN KEY (patientId) REFERENCES Patients(patientId);
-
-ALTER TABLE Appointments
-  ADD CONSTRAINT fk_appointment_doctor FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId);
+ 
 
 -- Medical Record
 
 CREATE TABLE MedicalRecords (
-  recordId SERIAL PRIMARY KEY,
+  recordId INT AUTO_INCREMENT PRIMARY KEY, 
   diagnosis TEXT NOT NULL,
   prescriptions TEXT,
   testResults TEXT,
-  date DATEONLY NOT NULL,
-  patientId INTEGER REFERENCES Patients(patientId),
-  doctorId INTEGER REFERENCES Doctors(doctorId)
+  date DATE NOT NULL,
+  patientId INT UNSIGNED,
+  doctorId INT UNSIGNED,
+  FOREIGN KEY (patientId) REFERENCES Patients(patientId),
+  FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId)
 );
 
-ALTER TABLE MedicalRecords
-  ADD CONSTRAINT fk_medical_record_patient FOREIGN KEY (patientId) REFERENCES Patients(patientId);
-
-ALTER TABLE MedicalRecords
-  ADD CONSTRAINT fk_medical_record_doctor FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId);
+ 
 
 -- Billing
 
 CREATE TABLE Billings (
-  billId SERIAL PRIMARY KEY,
+  billId INT AUTO_INCREMENT PRIMARY KEY, 
   totalAmount DECIMAL(10, 2) NOT NULL,
   paymentStatus VARCHAR(255) NOT NULL,
-  patientId INTEGER REFERENCES Patients(patientId)
+  patientId INT UNSIGNED,
+  FOREIGN KEY (patientId) REFERENCES Patients(patientId)
 );
 
-ALTER TABLE Billings
-  ADD CONSTRAINT fk_billing_patient FOREIGN KEY (patientId) REFERENCES Patients(patientId);
+ 
 
 -- Inventory
 
 CREATE TABLE Inventories (
-  itemId SERIAL PRIMARY KEY,
+  itemId INT AUTO_INCREMENT PRIMARY KEY, 
   itemName VARCHAR(255) NOT NULL,
-  quantity INTEGER NOT NULL,
-  expiryDate DATEONLY,
+  quantity INT NOT NULL,
+  expiryDate DATE,
   supplierInformation TEXT
 );
 
 -- AvailabilitySchedule
 
 CREATE TABLE AvailabilitySchedules (
-  availabilityId SERIAL PRIMARY KEY,
+  availabilityId INT AUTO_INCREMENT PRIMARY KEY, 
   day VARCHAR(255) NOT NULL,
   startTime TIME NOT NULL,
   endTime TIME NOT NULL,
-  doctorId INTEGER REFERENCES Doctors(doctorId)
+  doctorId INT UNSIGNED,
+  FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId)
 );
 
-ALTER TABLE AvailabilitySchedules
-  ADD CONSTRAINT fk_availability_schedule_doctor FOREIGN KEY (doctorId) REFERENCES Doctors(doctorId);
+
